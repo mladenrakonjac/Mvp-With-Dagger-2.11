c46c9c6e2388099b46fcd509ea265904fc2286c7
Mladen Rakonjac <mladenbp@gmail.com>
2017-08-19 00:16:21 +0200

    Application class added

---
+++ app/src/main/java/me/fleka/mvpwithdagger/MyApplication.java
@@ -0,0 +1,15 @@
+package me.fleka.mvpwithdagger;
+
+import android.app.Application;
+
+/**
+ * Created by Mladen Rakonjac on 8/19/17.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
