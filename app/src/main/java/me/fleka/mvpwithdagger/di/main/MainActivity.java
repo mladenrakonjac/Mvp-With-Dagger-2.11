2d00c431b26c58468488199e550a7c00f9a2e636
Mladen Rakonjac <mladenbp@gmail.com>
2017-08-19 02:25:40 +0200

    MainActivity implements view

--- app/src/main/java/me/fleka/mvpwithdagger/di/main/MainActivity.java
+++ app/src/main/java/me/fleka/mvpwithdagger/di/main/MainActivity.java
@@ -1,15 +1,24 @@
 package me.fleka.mvpwithdagger.di.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import dagger.android.support.DaggerAppCompatActivity;
 import me.fleka.mvpwithdagger.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends DaggerAppCompatActivity implements MainContract.View{
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.text);
     }
+
+    @Override
+    public void showNewText(String newText) {
+        textView.setText(newText);
+    }
+
 }
